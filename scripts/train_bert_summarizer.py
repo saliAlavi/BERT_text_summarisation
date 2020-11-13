06e3ed6651f87cb0911db1997bf27eac10e3b372
salialavi <sa.alavi.b@gmail.com>
2020-11-13 22:18:22 +0330

    add multigpu

--- scripts/train_bert_summarizer.py
+++ scripts/train_bert_summarizer.py
@@ -26,10 +26,7 @@ import re, string
 #mixed_precision.set_policy(policy)
 #optimizer = mixed_precision.LossScaleOptimizer(optimizer, loss_scale='dynamic')
 
-train_dataset, val_dataset, num_of_train_examples, _ = create_train_data()
-train_loss, train_accuracy = get_loss_and_accuracy()
-validation_loss, validation_accuracy = get_loss_and_accuracy()
-accumulators = []
+
 @tf.function(input_signature=train_step_signature)
 def train_step(input_ids, 
                input_mask, 
@@ -118,125 +115,134 @@ def check_ckpt(checkpoint_path):
         log.info('Training from scratch')
     return (ckpt_manager, latest_ckpt)
 
-# if a checkpoint exists, restore the latest checkpoint.
-ck_pt_mgr, latest_ckpt = check_ckpt(file_path.checkpoint_path)
-total_steps = int(h_parms.epochs * (h_parms.accumulation_steps))
-train_dataset = train_dataset.repeat(total_steps)
-count=0
+tf.debugging.set_log_device_placement(True)
 
-for (step, (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids)) in enumerate(train_dataset):
-  if step >= config.start_from_batch:
-    count+=1
-    start=time.time()
-    draft_mask = tf.math.logical_not(tf.math.equal(target_ids_[:, 1:], 0))
-    refine_mask = tf.math.logical_not(tf.math.equal(target_ids_[:, :-1], 0))
-    target_ids = label_smoothing(tf.one_hot(target_ids_, depth=config.input_vocab_size))
-    grad_accum_flag = True if (step+1)%h_parms.accumulation_steps == 0 else False
-    target_x, refine_predictions=train_step(
-                input_ids, 
-                input_mask, 
-                input_segment_ids, 
-                target_ids_, 
-                target_mask, 
-                target_segment_ids, 
-                target_ids, 
-                draft_mask,
-                refine_mask,
-                grad_accum_flag
-                )
-    if grad_accum_flag:
-      batch_run_check(
-                    step+1,  
-                    start, 
-                    train_summary_writer, 
-                    train_loss.result(), 
-                    train_accuracy.result(), 
-                    model
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
+
+    train_dataset, val_dataset, num_of_train_examples, _ = create_train_data()
+    train_loss, train_accuracy = get_loss_and_accuracy()
+    validation_loss, validation_accuracy = get_loss_and_accuracy()
+    accumulators = []
+    # if a checkpoint exists, restore the latest checkpoint.
+    ck_pt_mgr, latest_ckpt = check_ckpt(file_path.checkpoint_path)
+    total_steps = int(h_parms.epochs * (h_parms.accumulation_steps))
+    train_dataset = train_dataset.repeat(total_steps)
+    count=0
+
+    for (step, (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids)) in enumerate(train_dataset):
+      if step >= config.start_from_batch:
+        count+=1
+        start=time.time()
+        draft_mask = tf.math.logical_not(tf.math.equal(target_ids_[:, 1:], 0))
+        refine_mask = tf.math.logical_not(tf.math.equal(target_ids_[:, :-1], 0))
+        target_ids = label_smoothing(tf.one_hot(target_ids_, depth=config.input_vocab_size))
+        grad_accum_flag = True if (step+1)%h_parms.accumulation_steps == 0 else False
+        target_x, refine_predictions=train_step(
+                    input_ids,
+                    input_mask,
+                    input_segment_ids,
+                    target_ids_,
+                    target_mask,
+                    target_segment_ids,
+                    target_ids,
+                    draft_mask,
+                    refine_mask,
+                    grad_accum_flag
                     )
-    eval_frequency = ((step+1) * h_parms.batch_size) % config.eval_after
-    if eval_frequency == 0:
-      predicted = (tokenizer.decode([i for i in tf.squeeze(tf.argmax(refine_predictions,axis=-1)) if i not in [101,102,0]]))
-      target = (tokenizer.decode([i for i in tf.squeeze(target_x) if i not in [101,102,0]]))
-      print(f'the golden summary is {target}')
-      print(f'the predicted summary is {predicted if predicted else "EMPTY"}')
-      ckpt_save_path = ck_pt_mgr.save()
-      (val_acc, val_loss, rouge_score, bert_score) = calc_validation_loss(
-                                                                          val_dataset, 
-                                                                          step+1, 
-                                                                          val_step, 
-                                                                          valid_summary_writer, 
-                                                                          validation_loss, 
-                                                                          validation_accuracy
-                                                                          )
-      
-      latest_ckpt+=(step+1)
-      log.info(
-               model_metrics.format(
-                                    step+1, 
-                                    train_loss.result(), 
-                                    train_accuracy.result(),
-                                    val_loss, 
-                                    val_acc,
-                                    rouge_score, 
-                                    bert_score
-                                   )
-              )
-      log.info(evaluation_step.format(step+1, time.time() - start))
-      log.info(checkpoint_details.format(step+1, ckpt_save_path))
+        if grad_accum_flag:
+          batch_run_check(
+                        step+1,
+                        start,
+                        train_summary_writer,
+                        train_loss.result(),
+                        train_accuracy.result(),
+                        model
+                        )
+        eval_frequency = ((step+1) * h_parms.batch_size) % config.eval_after
+        if eval_frequency == 0:
+          predicted = (tokenizer.decode([i for i in tf.squeeze(tf.argmax(refine_predictions,axis=-1)) if i not in [101,102,0]]))
+          target = (tokenizer.decode([i for i in tf.squeeze(target_x) if i not in [101,102,0]]))
+          print(f'the golden summary is {target}')
+          print(f'the predicted summary is {predicted if predicted else "EMPTY"}')
+          ckpt_save_path = ck_pt_mgr.save()
+          (val_acc, val_loss, rouge_score, bert_score) = calc_validation_loss(
+                                                                              val_dataset,
+                                                                              step+1,
+                                                                              val_step,
+                                                                              valid_summary_writer,
+                                                                              validation_loss,
+                                                                              validation_accuracy
+                                                                              )
 
-      #Print metrics:
-      pattern = re.compile('[\W_]+')
-      infer_ckpt = '75'
-      ckpt = tf.train.Checkpoint(model=model)
-      ckpt.restore(
-          'ckpt_dir/content/drive/My Drive/Text_summarization/BERT_text_summarisation/Summarization_inference_ckps/ckpt-' + infer_ckpt).expect_partial()
+          latest_ckpt+=(step+1)
+          log.info(
+                   model_metrics.format(
+                                        step+1,
+                                        train_loss.result(),
+                                        train_accuracy.result(),
+                                        val_loss,
+                                        val_acc,
+                                        rouge_score,
+                                        bert_score
+                                       )
+                  )
+          log.info(evaluation_step.format(step+1, time.time() - start))
+          log.info(checkpoint_details.format(step+1, ckpt_save_path))
 
-      train_examples = examples['train']
-      train_dataset = map_batch_shuffle(train_examples, 100, split='train', shuffle=True, batch_size=1,
-                                        filter_off=False)
-      for (step, (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids)) in enumerate(
-              train_dataset):
-          sum_hyp = tokenizer.convert_ids_to_tokens([i for i in tf.squeeze(input_ids) if i not in [CLS_ID, SEP_ID, 0]])
-          ip_ids = tokenizer.encode(' '.join(sum_hyp))
-          preds_draft_summary, preds_refined_summary, refine_attention_dist = predict_using_beam_search(
-              tf.convert_to_tensor([ip_ids]),
-              refine_decoder_sampling_type='topktopp',
-              k=7,
-              p=0.8)
-          reference = tokenizer.convert_ids_to_tokens(
-              [i for i in tf.squeeze(target_ids_) if i not in [CLS_ID, SEP_ID, 0]])
-          reference = ' '.join(list(reference))
-          sum_hyp = tokenizer.convert_ids_to_tokens(
-              [i for i in tf.squeeze(preds_refined_summary) if i not in [CLS_ID, SEP_ID, 0]])
-          summary = convert_wordpiece_to_words(sum_hyp)
-          pattern = re.compile("[^A-Za-z ]")
-          summary = pattern.sub('', summary)
-          reference = pattern.sub('', reference)
-          pattern = re.compile("[ ]+")
-          summary = pattern.sub(' ', summary)
-          reference = pattern.sub(' ', reference)
-          rouge = Pythonrouge(summary_file_exist=False,
-                              summary=[[summary]], reference=[[[reference]]],
-                              n_gram=2, ROUGE_SU4=True, ROUGE_L=True,
-                              recall_only=True, stemming=True, stopwords=True,
-                              word_level=True, length_limit=True, length=50,
-                              use_cf=False, cf=95, scoring_formula='average',
-                              resampling=True, samples=1000, favor=True, p=0.5)
-          score = rouge.calc_score()
-          print(score)
-          score_avg = (score['ROUGE-1'] + score['ROUGE-2'] + score['ROUGE-L']) / 3
-          print(score_avg)
-          break
+          #Print metrics:
+          pattern = re.compile('[\W_]+')
+          infer_ckpt = '75'
+          ckpt = tf.train.Checkpoint(model=model)
+          ckpt.restore(
+              'ckpt_dir/content/drive/My Drive/Text_summarization/BERT_text_summarisation/Summarization_inference_ckps/ckpt-' + infer_ckpt).expect_partial()
+
+          train_examples = examples['train']
+          train_dataset = map_batch_shuffle(train_examples, 100, split='train', shuffle=True, batch_size=1,
+                                            filter_off=False)
+          for (step, (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids)) in enumerate(
+                  train_dataset):
+              sum_hyp = tokenizer.convert_ids_to_tokens([i for i in tf.squeeze(input_ids) if i not in [CLS_ID, SEP_ID, 0]])
+              ip_ids = tokenizer.encode(' '.join(sum_hyp))
+              preds_draft_summary, preds_refined_summary, refine_attention_dist = predict_using_beam_search(
+                  tf.convert_to_tensor([ip_ids]),
+                  refine_decoder_sampling_type='topktopp',
+                  k=7,
+                  p=0.8)
+              reference = tokenizer.convert_ids_to_tokens(
+                  [i for i in tf.squeeze(target_ids_) if i not in [CLS_ID, SEP_ID, 0]])
+              reference = ' '.join(list(reference))
+              sum_hyp = tokenizer.convert_ids_to_tokens(
+                  [i for i in tf.squeeze(preds_refined_summary) if i not in [CLS_ID, SEP_ID, 0]])
+              summary = convert_wordpiece_to_words(sum_hyp)
+              pattern = re.compile("[^A-Za-z ]")
+              summary = pattern.sub('', summary)
+              reference = pattern.sub('', reference)
+              pattern = re.compile("[ ]+")
+              summary = pattern.sub(' ', summary)
+              reference = pattern.sub(' ', reference)
+              rouge = Pythonrouge(summary_file_exist=False,
+                                  summary=[[summary]], reference=[[[reference]]],
+                                  n_gram=2, ROUGE_SU4=True, ROUGE_L=True,
+                                  recall_only=True, stemming=True, stopwords=True,
+                                  word_level=True, length_limit=True, length=50,
+                                  use_cf=False, cf=95, scoring_formula='average',
+                                  resampling=True, samples=1000, favor=True, p=0.5)
+              score = rouge.calc_score()
+              print(score)
+              score_avg = (score['ROUGE-1'] + score['ROUGE-2'] + score['ROUGE-L']) / 3
+              print(score_avg)
+              break
 
 
 
-      if not monitor_run(
-                         latest_ckpt, 
-                         ckpt_save_path, 
-                         val_loss, 
-                         val_acc, 
-                         bert_score, 
-                         rouge_score, 
-                         valid_summary_writer, 
-                         step+1):
-        break  
+          if not monitor_run(
+                             latest_ckpt,
+                             ckpt_save_path,
+                             val_loss,
+                             val_acc,
+                             bert_score,
+                             rouge_score,
+                             valid_summary_writer,
+                             step+1):
+            break
