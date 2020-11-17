67ee01d896aa55ecf43046f97613aad49e783dba
salialavi <sa.alavi.b@gmail.com>
2020-11-17 09:09:34 +0330

    some changes

--- scripts/train_bert_summarizer.py
+++ scripts/train_bert_summarizer.py
@@ -138,20 +138,20 @@ with strategy.scope():
     start = randint(ds_train_size - length, size=1)[0]
     train_buffer_size = 287113
     for (step, (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids)) in enumerate(train_dataset):
-      if step >= config.start_from_batch:
-          sum_hyp = tokenizer.convert_ids_to_tokens([i for i in tf.squeeze(input_ids) if i not in [CLS_ID, SEP_ID, 0]])
-          ip_ids = tokenizer.encode(' '.join(sum_hyp))
-          if len(ip_ids) >= 512:
-              while len(ip_ids) >= 512:
-                  start = randint(ds_train_size - length, size=1)[0]
-                  examples, metadata = tfds.load('cnn_dailymail', with_info=True, as_supervised=True,
+        if step >= config.start_from_batch:
+            sum_hyp = tokenizer.convert_ids_to_tokens([i for i in tf.squeeze(input_ids) if i not in [CLS_ID, SEP_ID, 0]])
+            ip_ids = tokenizer.encode(' '.join(sum_hyp))
+            if len(ip_ids) >= 512:
+                while len(ip_ids) >= 512:
+                    start = randint(ds_train_size - length, size=1)[0]
+                    examples, metadata = tfds.load('cnn_dailymail', with_info=True, as_supervised=True,
                                                  data_dir='/content/drive/My Drive/Text_summarization/cnn_dataset',
                                                  builder_kwargs={"version": "3.0.0"},
                                                  split=tfds.core.ReadInstruction('train', from_=start, to=start + length,
                                                                                  unit='abs'))
-                  train_examples = examples
-                  train_dataset = map_batch_shuffle(train_examples, train_buffer_size, split='train', shuffle=True, batch_size=1, filter_off=False)
-                  (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids) =iter(train_dataset)
+                    train_examples = examples
+                    train_dataset = map_batch_shuffle(train_examples, train_buffer_size, split='train', shuffle=True, batch_size=1, filter_off=False)
+                    (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, target_segment_ids) =iter(train_dataset)
 
         count+=1
         start=time.time()
@@ -249,13 +249,13 @@ with strategy.scope():
 
 
 
-          if not monitor_run(
-                             latest_ckpt,
-                             ckpt_save_path,
-                             val_loss,
-                             val_acc,
-                             bert_score,
-                             rouge_score,
-                             valid_summary_writer,
-                             step+1):
-            break
+        if not monitor_run(
+                         latest_ckpt,
+                         ckpt_save_path,
+                         val_loss,
+                         val_acc,
+                         bert_score,
+                         rouge_score,
+                         valid_summary_writer,
+                         step+1):
+        break
